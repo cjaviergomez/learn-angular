5153b4507c35a3cd38188e32a740858e7daa29f0
Carlos Gomez <javiergomez0214@gmail.com>
2022-11-17 21:44:00 -0500

    feat: add faker to generate mocks

--- src/app/modules/testing/services/product.service.spec.ts
+++ src/app/modules/testing/services/product.service.spec.ts
@@ -1,9 +1,13 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
+import { generateManyProducts } from '../../../core/models/product.mock';
+import { Product } from '../../../core/models/product.model';
 import { ProductsService } from './product.service';
 
 describe('ProductsService', () => {
 	let productService: ProductsService;
+	let httpController: HttpTestingController;
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
@@ -12,9 +16,29 @@ describe('ProductsService', () => {
 		});
 
 		productService = TestBed.inject(ProductsService);
+		httpController = TestBed.inject(HttpTestingController);
 	});
 
 	it('should be created', () => {
 		expect(productService).toBeTruthy();
+	});
+
+	describe('Tests for getAllSimple', () => {
+		it('should return a product list', doneFn => {
+			// Arrange
+			const mockProducts: Product[] = generateManyProducts(2);
+			// Act
+			productService.getAllSimple().subscribe(products => {
+				// Assert
+				expect(products).toEqual(mockProducts);
+				doneFn();
+			});
+
+			// http config
+			const url = `${environment.API_URL}/api/v1/products`;
+			const req = httpController.expectOne(url);
+			req.flush(mockProducts);
+			httpController.verify();
+		});
 	});
 });
