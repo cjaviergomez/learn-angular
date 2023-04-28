878f7dc12c4978761e61903414fd05bd73e50b17
Carlos Gomez Aguilar <gomezaguilar.20@nlsa.teleperformance.com>
2023-04-27 21:01:09 -0500

    feat: test component with input and simulate a click

--- src/app/modules/testing/components/person/person.component.spec.ts
+++ src/app/modules/testing/components/person/person.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Person } from '../../classes/person.model';
 import { PersonComponent } from './person.component';
 
 fdescribe('PersonComponent', () => {
@@ -22,19 +23,51 @@ fdescribe('PersonComponent', () => {
 		expect(component).toBeTruthy();
 	});
 
-	it('should have a <p> with "Person works!"', () => {
+	it('should have a person name property equal to "Carlos"', () => {
+		// Input property
+		component.person = new Person('Carlos', 'Perez', 25, 1.75, 80);
+		expect(component.person.name).toEqual('Carlos');
+	});
+
+	it('should have a <p> that contain "{{ person.height }}"', () => {
+		// Arrange
+		component.person = new Person('Javier', 'Perez', 25, 1.72, 80);
+		const mockMessage = `Your height is ${component.person.height}`;
 		const personDebugElement: DebugElement = fixture.debugElement; // Buena practica usar el DebugElement
 		const pDebug: DebugElement = personDebugElement.query(By.css('p'));
 		// const compiled: HTMLElement = fixture.nativeElement;
 		// const compiled: HTMLElement = personDebugElement.nativeElement;
 		const pElement: HTMLElement = pDebug.nativeElement;
-		expect(pElement?.textContent).toEqual('Person works!');
+		// Act
+		fixture.detectChanges();
+		// Assert
+		expect(pElement?.textContent).toContain(component.person.height);
 	});
 
-	it('should have a <h1> with "Person"', () => {
+	it('should have a <h1> with "Hi, {person.name}"', () => {
+		// Arrange
+		component.person = new Person('Javier', 'Perez', 25, 1.75, 80);
+		const mockMessage = `Hi, ${component.person.name}`;
 		const personDebugElement: DebugElement = fixture.debugElement;
 		const h1Debug: DebugElement = personDebugElement.query(By.css('h1'));
 		const h1Element: HTMLElement = h1Debug.nativeElement;
-		expect(h1Element?.textContent).toEqual('Person');
+		// Act
+		fixture.detectChanges();
+		// Assert
+		expect(h1Element?.textContent).toEqual(mockMessage);
+	});
+
+	it('should display a text with IMC when do click"', () => {
+		// Arrange
+		component.person = new Person('Javier', 'Perez', 25, 1.75, 50);
+		const mockMessage = `down`;
+		const personDebugElement: DebugElement = fixture.debugElement;
+		const buttonDebug: DebugElement = personDebugElement.query(By.css('button.btn-imc'));
+		const buttonElement: HTMLElement = buttonDebug.nativeElement;
+		// Act
+		buttonDebug.triggerEventHandler('click', null);
+		fixture.detectChanges();
+		// Assert
+		expect(buttonElement?.textContent).toContain(mockMessage);
 	});
 });
--- src/app/modules/testing/components/person/person.component.ts
+++ src/app/modules/testing/components/person/person.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Person } from '../../classes/person.model';
 
 @Component({
-  selector: 'app-person',
-  templateUrl: './person.component.html',
-  styleUrls: ['./person.component.css']
+	selector: 'app-person',
+	templateUrl: './person.component.html',
+	styleUrls: ['./person.component.css']
 })
 export class PersonComponent implements OnInit {
+	@Input() person: Person = new Person('Carlos', 'Perez', 25, 1.72, 80);
+	imc = '';
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	calcIMC(): void {
+		this.imc = this.person.calcIMC();
+	}
 }
